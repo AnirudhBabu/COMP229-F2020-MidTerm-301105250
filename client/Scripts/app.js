30d1e3df20acaa667c14957b3fb33c77b27b9019
Anirudh Babu <anirudhbabu2001@gmail.com>
2020-10-26 21:28:25 -0400

    Added edit and delete buttons

--- client/Scripts/app.js
+++ client/Scripts/app.js
@@ -105,12 +105,51 @@ $.fn.pageMe = function(opts){
 
 // IIFE
 (function(){
-  $(".btn-danger").click(function(event){
-    if(!confirm("Are you sure?")) {
-      event.preventDefault();
-      window.location.assign("/games");
-    }
-  });
+    if(document.title == "Books")
+    {
+        let deleteButtons = document.getElementsByClassName("btn-danger");
+        
+        for (let index = 0; index < deleteButtons.length; index++) {
+            let button = deleteButtons[index];
+            button.addEventListener("click", (event) => {
+                event.preventDefault();
+                document.getElementsByTagName("body")[0].innerHTML += `<!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop" style="visibility:hidden;" id="modalActivator"></button>
+                
+                <!-- Modal -->
+                <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <span><h5 class="modal-title" id="staticBackdropLabel">Confirm</h5></span>
+                        <span><button type="button" class="close" data-dismiss="modal" aria-label="Close"></span>
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Are you sure?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal" id="cancelButton">Cancel</button>
+                        <button type="button" class="btn btn-primary" id="confirmButton">Yes</button>
+                    </div>
+                    </div>
+                </div>
+                </div>`;
+                modalActivator.click();
+                document.getElementById("confirmButton").addEventListener("click", () => 
+                {
+                    window.location.href = button.getAttribute("href");
+                });
+                document.getElementById("cancelButton").addEventListener("click", () =>{
+                    window.location.href = "/books";
+                });
+            });
+            
+        }
+        
+  
+    };
 
  /* pagination code */
   $('#myTable').pageMe(
--- server/routes/books.js
+++ server/routes/books.js
@@ -16,7 +16,11 @@ router.get('/', (req, res, next) => {
     else {
       res.render('books/index', {
         title: 'Books',
-        books: books
+        books: books.sort((a, b) => 
+        {
+          let sorted = [a.Title, b.Title].sort();
+          return sorted[0] == a.Title ? -1 : 1;
+        })
       });
     }
   });
@@ -25,17 +29,16 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-  
+
   res.render('books/details', {
     title: 'Book Details',
-    books: {Title: "", Price: "", Author: "", Genre: ""}
+    book: {Title: "", Price: "", Author: "", Genre: ""}
   });
 
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
-  
   let bookToCreate = book({
     "Title": req.body.Title,
     "Author": req.body.Author,
@@ -60,28 +63,62 @@ router.post('/add', (req, res, next) => {
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
+router.get('/edit/:id', (req, res, next) => {
+  let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    // pass id to the db 
+    book.findById(id, (err, bookToUpdate) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        // show the update view
+        res.render('books/details', 
+        {
+            title: 'Edit Book',
+            book: bookToUpdate,
+            id: id
+        });
+    });
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
+router.post('/edit/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let id = req.params.id;
+
+  book.updateOne({_id: id},
+  {
+      "_id": id,
+      "Title": req.body.Title,
+      "Author": req.body.Author,
+      "Price": req.body.Price,
+      "Genre": req.body.Genre  
+  }, (err) => {
+      if(err)
+      {
+          console.log(err);
+          res.end(err);
+      }
+      res.redirect('/books');
+  });
 
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+  let id = req.params.id;
+
+  book.remove({_id: id}, (err) => {
+      if(err)
+      {
+          console.log(err);
+          res.end(err);
+      }
+      res.redirect('/books');
+  });
 });
 
 
