deacbd7f34d1f31ff34835384aba22d9be0bc991
Anirudh Babu <anirudhbabu2001@gmail.com>
2020-10-28 17:28:13 -0400

    Edited the route to edit page

--- server/routes/books.js
+++ server/routes/books.js
@@ -69,7 +69,7 @@ router.post('/add', (req, res, next) => {
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/edit/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   let id = req.params.id;
 
     // pass id to the db 
@@ -90,7 +90,7 @@ router.get('/edit/:id', (req, res, next) => {
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/edit/:id', (req, res, next) => {
+router.post('/:id', (req, res, next) => {
 
   let id = req.params.id;
 
